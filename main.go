3daf3caca9a40cf56c21d2c7c6c5e2a823a04535
Miriam <miriam.gil@lifullconnect.com>
2019-09-29 20:16:46 +0200

    possible algorithm idea!

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 
 }
 
 //WardrobeCombinator wardrobe combinator
 func WardrobeCombinator() [][]int {
-
-	a := [][]int{{50, 50, 50, 50, 50}, {100, 100, 50}, {75, 75, 100}}
+	sizes := []int{120, 100, 75, 50}
+	//a := [][]int{{50, 50, 50, 50, 50}, {100, 100, 50}, {75, 75, 100}}
+	sizesAux := [][]int{}
+	for _, s := range sizes {
+		sizesAux = append(sizesAux, mixer(sizes, s))
+	}
 
 	//Idea of possible solution in pseudo-code
 	// []int solution1= mi_algoritmo()
@@ -17,5 +23,20 @@ func WardrobeCombinator() [][]int {
 	// 	a.add(solution1)
 	// }
 
-	return a
+	return sizesAux
+}
+
+func mixer(sizes []int, num int) []int {
+	long := 250
+	longAux := 0
+	sizesAux := []int{}
+	for _, i := range sizes {
+		longAux = num + i
+		if longAux-long >= 50 {
+			sizesAux = append(sizesAux, i)
+			fmt.Println(sizesAux)
+		}
+	}
+
+	return sizesAux
 }
